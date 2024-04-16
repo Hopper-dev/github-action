d4f2526fea40d57e8d2413e5c27763e844b51674
Michael Peleg <michaelphop@hopper.security>
2024-04-16 20:00:37 +0300

    push

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
+import glob
 import os
 
-import requests
-import sys
 
-def download_file(url, file_path):
-    # if not url:
-    #     raise ValueError("URL is required")
-    # if not file_path:
-    #     raise ValueError("File path is required")
-    # response = requests.get(url)
-    # response.raise_for_status()
-    # with open(file_path, 'wb') as f:
-    #     f.write(response.content)
-    # print(f"Downloaded {url} to {file_path}")
-    print("in download_file!")
+def findAllFilesWithSuffixInDir(suffix: str, directory: str):
+    pattern = os.path.join(directory, '**', suffix)
+
+    # Find all files matching the pattern
+    jar_files = glob.glob(pattern, recursive=True)
+
+    return jar_files
+
 
 if __name__ == '__main__':
-    print(f"in __main__!, pwd is: {os.getcwd()}")
-    url = sys.argv[1] if len(sys.argv) > 1 else None
-    file_path = sys.argv[2] if len(sys.argv) > 2 else None
-    download_file(url, file_path)
+    jar_files = findAllFilesWithSuffixInDir("*.jar", os.getcwd())
+    print(f"jar_files=[{jar_files}]")
